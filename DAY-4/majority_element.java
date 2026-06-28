4231fb8f258b86a9bf2caedf5ff8a5078e48ba26
Shruti Mishra <shrutimishra5020@gmail.com>
2026-06-29 00:02:52 +0530

    Add comments for majority element solution
    Added comments for problem statement and approach in majority_element.java.

---
+++ DAY-4/majority_element.java
@@ -0,0 +1,28 @@
+LEETCODE SOLN:169
+APPROACH =BRUTE FORCE METHOD
+S.C=O(n^2)
+
+--------------------------------------------------------------------------------------------------------------------------------------
+  
+class Solution {
+    public int majorityElement(int[] nums) {
+        int max=(nums.length)/2;
+        // int maj;
+        int m=max;
+        for(int i=0;i<nums.length;i++){
+            int count=1;
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]==nums[i]){
+                    count++;
+                }
+                
+            }
+            if(count>m){
+            m=count;
+            if(count>=max )
+           return nums[i];
+            }
+        }
+       return -1; 
+    }
+}
