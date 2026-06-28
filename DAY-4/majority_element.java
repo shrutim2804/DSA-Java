052b40df803cfe399e84f96f5946f66bc81da87c
Shruti Mishra <shrutimishra5020@gmail.com>
2026-06-29 00:04:08 +0530

    Remove commented code in majorityElement method
    Removed commented variable declaration for clarity.

--- DAY-4/majority_element.java
+++ DAY-4/majority_element.java
@@ -7,7 +7,6 @@ S.C=O(n^2)
 class Solution {
     public int majorityElement(int[] nums) {
         int max=(nums.length)/2;
-        // int maj;
         int m=max;
         for(int i=0;i<nums.length;i++){
             int count=1;
