83e1ffa89527dd893009b473a47ecf5abbe3bfeb
Shruti Mishra <shrutimishra5020@gmail.com>
2026-06-29 22:16:16 +0530

    Create remove_element.java

---
+++ DAY-5/remove_element.java
@@ -0,0 +1,15 @@
+Leetcode qs=27
+
+  class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+        
+    }
+}
