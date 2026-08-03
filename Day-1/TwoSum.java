01b379c47c136dad21fe84537072d294e75f4efd
Shruti Mishra <shrutimishra5020@gmail.com>
2026-08-03 21:40:22 +0530

    Add twoSum method with HashMap implementation
    Implemented twoSum method using HashMap for improved efficiency.

--- Day-1/TwoSum.java
+++ Day-1/TwoSum.java
@@ -18,4 +18,24 @@ Problem: Two Sum
     }
 }
 
-  
+
+
+__________________________________________________________________________________________________________________________________________________
+
+
+ THROUGH HASHMAP
+________________________
+
+  class Solution {
+    public int[] twoSum(int[] nums, int target) {
+       HashMap<Integer, Integer> m=new HashMap<>();       
+        for(int i=0;i<nums.length;i++){
+            int need=target-nums[i];
+            if(m.containsKey(need)){
+                return new int[]{m.get(need),i};
+            }
+            m.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+}
