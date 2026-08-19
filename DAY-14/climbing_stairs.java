2984652234b6e68e413b0d3cec23c96c7250ddfc
Shruti Mishra <shrutimishra5020@gmail.com>
2026-08-20 00:42:51 +0530

    Correct climbing stairs logic and array initialization
    Fix the initialization of the stairs array and update the loop to start from index 2.

--- DAY-14/climbing_stairs.java
+++ DAY-14/climbing_stairs.java
@@ -5,8 +5,7 @@ class Solution {
         int[] s=new int[45];
         s[0]=1;
         s[1]=2;
-        s[2]=3;
-        for(int i=3;i<45;i++){
+        for(int i=2;i<45;i++){
             s[i]=s[i-2]+s[i-1];
         }
 
